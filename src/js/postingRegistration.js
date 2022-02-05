0c0cb98780a7c5e20741e3c221efe757f9051429
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-05 18:43:38 +0100

    Comments to the sendingData.js

--- src/js/postingRegistration.js
+++ src/js/postingRegistration.js
@@ -1,3 +1,5 @@
+//creating a function that posts the data to the API
+
 export const postingRegistration = async (
   id,
   name,
@@ -7,7 +9,9 @@ export const postingRegistration = async (
   age,
   comment
 ) => {
+  //link where to send the data
   const url = `https://test-api.codingbootcamp.cz/api/02605eae/events/${id}/registrations`;
+  //the data
   const data = {
     id: id,
     name: name,
@@ -19,15 +23,17 @@ export const postingRegistration = async (
   };
 
   try {
+    //trying to post them
     const post = await fetch(url, {
       method: "POST",
       body: JSON.stringify(data),
       headers: { "Content-Type": "application/json" },
     });
-
+    //converting the response into JS  and console.log whether the post was successful
     const response = await post.json();
     console.log(response);
   } catch (e) {
+    //catching the potential error
     console.log(`There was an error: ${e}`);
   } finally {
   }
