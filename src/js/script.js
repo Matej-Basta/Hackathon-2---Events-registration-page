48ce71147e13e157f6039ee888d616a985ed9698
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-05 18:19:15 +0100

    Comments in script.js

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,6 @@ import { gettingData } from "./fetching.js";
 
 //waiting for the DOM to load
 document.addEventListener("DOMContentLoaded", () => {
-  //initial creation of elements
+  //initial creation of elements when the page loads
   gettingData();
 });
